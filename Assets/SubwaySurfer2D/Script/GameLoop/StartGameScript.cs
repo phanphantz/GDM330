2ff8fece377e5b4ca273aea164b8888148308020
Phun Peeticharoenthum <phunphun484@gmail.com>
2023-08-26 02:45:57 +0700

    ADD: SubwaySurfer2D Game

---
+++ Assets/SubwaySurfer2D/Script/GameLoop/StartGameScript.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace SuperGame.SubwaySurfer2D
+{
+    public class StartGameScript : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Time.timeScale = 0;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                Time.timeScale = 1.0f;
+                this.gameObject.SetActive(false);
+            }
+        }
+    }
+}
